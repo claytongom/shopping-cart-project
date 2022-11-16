2348874abb9e9dea695f1aed761792aee84d5cfe
claytongom <claytoncnx@gmail.com>
2022-11-16 19:43:55 -0300

    resolução requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const productsList = document.querySelector('.products');
+const products = await fetchProductsList('computador');
+products.forEach((product) => {
+  const productElement = createProductElement(product);
+  productsList.appendChild(productElement);
+});
