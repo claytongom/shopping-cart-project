f676261507b6a051aa2f2fa6485e7a7d915feb7b
claytongom <claytoncnx@gmail.com>
2022-11-16 20:55:58 -0300

    resolução requisito 4

--- src/main.js
+++ src/main.js
@@ -6,7 +6,19 @@ import './style.css';
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const productsList = document.querySelector('.products');
+
+const loadMessage = document.createElement('p');
+loadMessage.className = 'loading';
+loadMessage.innerText = 'carregando...';
+productsList.appendChild(loadMessage);
+
+const clearLoadingText = () => {
+  const loadingText = document.querySelector('.loading');
+  loadingText.remove();
+};
+
 const products = await fetchProductsList('computador');
+clearLoadingText();
 products.forEach((product) => {
   const productElement = createProductElement(product);
   productsList.appendChild(productElement);
