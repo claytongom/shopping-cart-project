20431c25c374a5e73f765797ef2ba0e9874d818e
claytongom <claytoncnx@gmail.com>
2022-11-25 12:42:11 -0300

    resolução requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,20 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (cep) => {
+  const newAddreess = await Promise.any([
+    fetch(`https://cep.awesomeapi.com.br/json/${cep}`),
+    fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`),
+  ]);
+
+  const infos = await newAddreess.json();
+  return infos;
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const yourCep = document.querySelector('.cep-input').value;
+  const address = document.querySelector('.cart__address');
+  try {
+    const dat = await getAddress(yourCep);
+    address.innerText = `${dat.address} - ${dat.district} - ${dat.city} - ${dat.state}`;
+  } catch (error) {
+    address.innerHTML = 'CEP não encontrado';
+  }
 };
